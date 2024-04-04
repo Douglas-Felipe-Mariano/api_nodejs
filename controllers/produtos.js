4c0997eb3e9b84c048abf7bd54c568f51d1576ee
Douglas-Felipe-Mariano <159494725+Douglas-Felipe-Mariano@users.noreply.github.com>
2024-04-04 20:23:25 -0300

    aaaaaaaaaa

--- controllers/produtos.js
+++ controllers/produtos.js
@@ -4,11 +4,23 @@ const db = require('../database/conection');
 module.exports = {
     async listarProdutos(request, response) {
         try {
+
+            const sql = `SELECT 
+            prd_id, prd_nome, prd_valor, prd_unidade, 
+            prd_disponivel, prd_img, prd_img_destaque, 
+            prd_descricao, prd_img_destaque 
+            FROM produtos 
+            WHERE prd_disponivel=1;`
+
+            const produtos = await db.query(sql)
+            const nItems = produtos[0].length;
+
             // throw new Error('Eu causei o erro!');
             return response.status(200).json({
                 sucesso: true, 
                 mensagem: 'Lista de produtos.', 
-                dados: null
+                dados: produtos[0],
+                nItems
             });
         } catch (error) {
             return response.status(500).json({
--- routes/routes.js
+++ routes/routes.js
@@ -18,7 +18,7 @@ const ProdutoTiposController = require('../controllers/produtoTipos');
 router.get('/usuarios', UsuariosController.listarUsuarios); 
 router.post('/usuarios', UsuariosController.cadastrarUsuarios); 
 router.patch('/usuarios', UsuariosController.editarUsuarios); 
-router.delete('/usuarios/:usu_id', UsuariosController.apagarUsuarios); 
+router.delete('/usuarios', UsuariosController.apagarUsuarios); 
 
 router.get('/produtos', ProdutosController.listarProdutos); 
 router.post('/produtos', ProdutosController.cadastrarProdutos); 
